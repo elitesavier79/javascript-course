3456216b97e2bf925a2ab570fbbe220d6019610e
elitesavier79 <elitesavier79@gmail.com>
2020-07-04 11:11:09 +0700

    penulisan code di dalam javascript

---
+++ formatCode.js
@@ -0,0 +1,34 @@
+Penulisan Format code di dalam javascript
+
+=>penulisan variabel 
+
+1.dengan camelCase
+var thisIsFunction = "this is Function"
+var headerTitle = " Belajar javascript"
+
+2.dengan Upcase (untuk variabel yang ber nilai konstan)
+const PI = 3.1
+
+=>penulisan statment
+var x = y + z ;
+
+
+=>penulisan indentation (mengunakan tab)
+Function sumNum(a,d) {
+	return a + b;
+}
+
+
+=>penulisan harus sensitive Case
+var iniSebuahVar = "var";
+iniSebuahVar ="variable baru";
+
+=> penulisan array
+var array = [
+"bubur ayam",
+"nasi goreng",
+2,
+10,
+2000,
+"1999"
+];
