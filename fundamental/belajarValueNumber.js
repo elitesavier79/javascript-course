49e990c4cdf09aa4f88e30b2d03774032d78f7d9
elitesavier79 <elitesavier79@gmail.com>
2020-07-05 21:51:50 +0700

    Belajar number dan special number value berserta aritmatikanya

---
+++ fundamental/belajarValueNumber.js
@@ -0,0 +1,29 @@
+let number=2020;
+let desimal=30.98;
+
+let bigVeryNumber =1.889e8 //sama dengan 188.900.000
+
+//arithmetic operator
+let additional = 1000+ 500; //pemjumlahan
+let substraction = 1000-500; //pengurangan
+let multiplication = 100*5; // perkalian
+let division = 100/5; // pembagian
+let remainder = 320%100; // modulus/sisa bagi
+
+// javascript mengikuti order of arithmetics
+
+let edgeCase = 100 - 50 + 4 * 2;
+
+// Special Number in Javascript
+
+let specialNum1 = Infinity;
+let specialNum2 = -Infinity;
+let specialNum3 = NaN;
+
+//contoh pemakaian
+
+let specialCase1 = Infinity - 1;
+let specialCase2 = "five" - 1;
+
+console.log(specialCase2);
+
