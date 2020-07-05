72befdeb15365cf9b34d6db23391991404075b85
elitesavier79 <elitesavier79@gmail.com>
2020-07-05 20:58:08 +0700

    belajar const variabel di javascript

---
+++ fundamental/belajarConst.js
@@ -0,0 +1,17 @@
+
+// kalo kita mengunakan let nilai masih bisa di ubah2
+//let firstName = "budi";
+
+//firstName =" udin";
+
+//console.log(firstName)
+
+// mengunakan const
+
+const firstName = "budi";
+
+//firstName= "udin";
+
+console.log(firstName);
+
+// arti kalo pakai const nilai otomatis konstan tidak dapaet di ubah
