6265136595fcf4830c0f18fab793f9cdfaae4096
elitesavier79 <elitesavier79@gmail.com>
2020-07-05 12:09:48 +0700

    belajar perbedaan var dan let di javascript

---
+++ fundamental/belajarVar.js
@@ -0,0 +1,9 @@
+var angka1=100;
+var angka2 = angka1+50;
+
+console.log(angka2);
+
+var nama="Dicky Swardi";
+var salam="Hello, ";
+
+console.log(salam+nama);
---
+++ fundamental/belajarVarDanLet.js
@@ -0,0 +1,6 @@
+
+let name="wegodev";
+console.log(name);
+// beda var sama let adalah let harus di initialization kalo var tidak perlu
+
+
