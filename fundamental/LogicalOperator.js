85f391b4308e1c7c68afe79132b43e4dfff452f5
elitesavier79 <elitesavier79@gmail.com>
2020-07-06 22:54:42 +0700

    belajar logical operator dan advanced comparison

---
+++ fundamental/LogicalOperator.js
@@ -0,0 +1,22 @@
+let name ="Wegodev";
+let angka = 7;
+
+
+//console.log(name === "wegodev"); // kalo menggunakan sama dengan 3 =>strict comparison
+//console.log(name == "wegodev"); // kalo menggunkan sama dengan 2  =>loose comparison
+
+
+//console.log(angka == "7"); // hasil nya true
+//console.log(angka === "7"); // hasil nya false
+
+//logical operator AND (&&)
+console.log(name === "wegodev" && angka === 7);
+
+//logical operator OR (||)
+console.log(name === "wegodev" || angka === 6);
+
+//logical operator NOT (!==)
+console.log(name !== "wegodev");
+
+
+
