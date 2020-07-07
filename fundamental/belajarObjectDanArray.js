68d4e5ec409636bdca8ff7aebe362d27363308c2
elitesavier79 <elitesavier79@gmail.com>
2020-07-07 11:03:52 +0700

    belajar dasar object dan Array di javascript

---
+++ fundamental/belajarObjectDanArray.js
@@ -0,0 +1,22 @@
+//penulisan Array
+
+let listmenu = ["mie ayama", "Burger", "hotdog"]; // pakai tanda kurung siku
+
+
+//penulisan Object
+let myBiodata = { // pakai tanda kurung kurawal
+	nama: "dicky",
+	makananFavorit: ["mie ayama", "Burger", "hotdog"],
+	istri: {
+		nama: "siawying",
+		makananFavorit: ["mie ayama", "Burger", "hotdog"]
+	},
+	anak: {
+		nama: "darren",
+		makananFavorit: ["mie ayama", "Burger", "hotdog"]
+	}
+
+}
+
+console.log(listmenu[1]);
+
