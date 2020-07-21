86ec1f0382f0f7e34cf0d3e649d11f1c3c5c485d
elitesavier79 <elitesavier79@gmail.com>
2020-07-21 13:32:13 +0700

    belajar looping/pengulangan pada javascript

---
+++ fundamental/belajarLoopingFor.js
@@ -0,0 +1,12 @@
+//looping for
+
+for (let i = 0; i<= 8; i= i+2) {// arti nya i pertama batas awal ; i kedua kondisi
+	console.log(i)
+}
+
+//contoh lain
+
+for (let i = "*"; i.length <= 5; i = i + "*") {
+	console.log(i) 
+}
+
---
+++ fundamental/belajarLoopingWhileAndDo.js
@@ -0,0 +1,22 @@
+//Tanpa looping
+console.log(0);
+console.log(2);
+console.log(4);
+console.log(6);
+console.log(8);
+
+//dengan looping while
+let count = 0;
+
+//while (count <= 8) { // selama count masih di bawah 8 lakukan code yg dibawah
+	//console.log(count)
+	//count = count + 2;
+
+ //}
+
+//dengan looping do while
+do {
+	console.log(count);
+	count = count + 2;
+} while (count <= 8);
+
