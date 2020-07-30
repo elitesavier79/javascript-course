9c8fc2f18e0a6cca81f93645e2ad0e9928b8979b
elitesavier79 <elitesavier79@gmail.com>
2020-07-30 13:08:50 +0700

    belajar function definition dan declaration

--- fundamental/belajarFunction.js
+++ fundamental/belajarFunction.js
@@ -1,4 +1,23 @@
 //function return value
 
-console.log(Math.min(19, 2, 8, 29))// contoh function dengan retrun value
-alert("ini adalah sebuah alert");// contoh function dengan side effect
+
+//function definition
+/*const perpangkatan = function(n) {
+	return n * n
+}
+console.log(perpangkatan(2))*/
+
+//function declaration
+function perpangkatan(n) {
+	return n*n
+}
+console.log(perpangkatan(3))
+
+//function side effect
+function sayhello() {
+	alert("hello")
+}
+console.log(sayhello())
+
+
+
