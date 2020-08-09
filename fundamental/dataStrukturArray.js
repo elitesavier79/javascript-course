cde852344c79d7c67aa216d98247fd3bf298b633
elitesavier79 <elitesavier79@gmail.com>
2020-08-09 11:25:08 +0700

    belajar property length pada array

--- fundamental/dataStrukturArray.js
+++ fundamental/dataStrukturArray.js
@@ -1,2 +1,13 @@
-const array = [2,5,6,10,19,28,"wegodev",true]
+//array properties
+//property LENGTH => gunakan untuk loop array
+
+const array = [2,5,6,10,19,28] // untuk array index mulai dari 0
+for (let i = 0; i < array.length ; i++) {
+	array[i] = array[i] * 2
+}
+
+//console.log(array.length) => untuk length index mulai dari 
+
+console.log(array[0] = "dua")
+console.log(array) 
 
