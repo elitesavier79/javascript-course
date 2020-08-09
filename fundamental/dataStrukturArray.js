222a9af76203cae20e2fc9cf56870fb873a890d5
elitesavier79 <elitesavier79@gmail.com>
2020-08-09 11:54:46 +0700

    belajar array method push,pop,shift dan unshift

--- fundamental/dataStrukturArray.js
+++ fundamental/dataStrukturArray.js
@@ -1,7 +1,7 @@
 //array properties
 //property LENGTH => gunakan untuk loop array
 
-const array = [2,5,6,10,19,28] // untuk array index mulai dari 0
+/*const array = [2,5,6,10,19,28] // untuk array index mulai dari 0
 for (let i = 0; i < array.length ; i++) {
 	array[i] = array[i] * 2
 }
@@ -9,5 +9,24 @@ for (let i = 0; i < array.length ; i++) {
 //console.log(array.length) => untuk length index mulai dari 
 
 console.log(array[0] = "dua")
-console.log(array) 
+console.log(array) */
 
+
+//array Methods
+
+const array = [1,2,3,4,5]
+
+//Push method => digunakan untuk menambah/add element/value baru di akhir array
+array.push(6)
+
+
+//pop method => digunakan untuk menghilangkan/remove element/value di akhir array
+array.pop()
+
+//unshift method => digunkan untuk menambah/add element/value di awal array
+array.unshift(0)
+
+//shift method => digunakan untuk menghilang/remove element/value di awal array
+array.shift()
+
+console.log(array)
