2c2a7606ea7873c3e06b451f5470d5f6ff199443
elitesavier79 <elitesavier79@gmail.com>
2020-08-16 12:00:28 +0700

    belajar Global dan lokal(function) Scope in javascript

---
+++ Functional Programming/javascriptScope.js
@@ -0,0 +1,19 @@
+//Global Scope => variable/funcion nya yang dapat diakses oleh code yg di buat baik di luar atau di dalam function
+
+const varGlobal = "ini contoh global scope";
+
+alert (varGlobal);
+
+
+
+
+//local/function Scope =>variable/function nya yang hanya dapat di akses oleh code di dalam function saja
+const localScope = function () {
+  const localVar = "ini contoh lokal scope";
+  alert(localVar);
+};
+
+localScope();
+
+
+ 
