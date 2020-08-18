f23943686516d91430f68fa4c007d9da9bdaea71
elitesavier79 <elitesavier79@gmail.com>
2020-08-18 11:51:41 +0700

    mengambil semua  UI elements dengan DOM API

--- mini-project/starter/app.js
+++ mini-project/starter/app.js
@@ -1,3 +1,7 @@
-const Heading = document.querySelector(".heading-title");
+const todoForm = document.querySelector("#todo-form");//untuk mengambil form input
+const todoInput = document.querySelector("#todo-input");// untuk mengambil tek input nya
+const filterInput = document.querySelector("#filter-input");// untuk mengambil test yang ada di filter tasks
+const todoList = document.querySelector("#todo-list");//untuk menampilkan list yg di pilih
+const clearButton = document.querySelector("#clear-todos");
 
-Heading.innerHTML = "Aplikasi TODOLIST";
+console.log(clearButton)
