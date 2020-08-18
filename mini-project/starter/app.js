f6268e1e9df2cd68636c8be5222c0d114f681116
elitesavier79 <elitesavier79@gmail.com>
2020-08-18 12:39:12 +0700

    membuat serta menambahkan class dan children pada element li

--- mini-project/starter/app.js
+++ mini-project/starter/app.js
@@ -4,4 +4,34 @@ const filterInput = document.querySelector("#filter-input");// untuk mengambil t
 const todoList = document.querySelector("#todo-list");//untuk menampilkan list yg di pilih
 const clearButton = document.querySelector("#clear-todos");
 
-console.log(clearButton)
+
+
+//AddEventListener adalah Merupakan method yang berfungsi untuk membuat event (click, change, mouseout, dll) untuk keperluan manipulasi DOM.
+todoForm.addEventListener("submit", addTodo);
+
+function addTodo(e) {
+	e.preventDefault();
+
+	//membuat li element
+	const li = document.createElement("li")
+
+	//menambahkan class pada element li
+	li.className = "list-group-item d-flex justify-content-between align-items-center mb-1"
+
+	//menambahkan children ke dlm element li
+	li.appendChild(document.createTextNode("Value dari task"))
+
+
+	//membuat delete button
+	const a = document.createElement("a");
+
+	//memberi properti untuk a element
+	a.href = "#";
+	a.className = "badge badge-danger"
+	a.innerHTML = "Delete";
+
+	//menyelipkan element a ke dlm children li
+	li.appendChild(a)
+
+	console.log(li)
+}
